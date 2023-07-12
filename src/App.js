627044f5a115a5981890c78e7f47bbc2ef3e17ff
Iain Hoolahan <iainhoolahan@outlook.com>
2023-07-12 11:37:20 +0100

    installing packages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import {
-  ChakraProvider,
   Box,
   Text,
   Link,
   VStack,
   Code,
   Grid,
-  theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
@@ -35,7 +32,6 @@ function App() {
           </VStack>
         </Grid>
       </Box>
-    </ChakraProvider>
   );
 }
 
