eb020cfca532abe6249284e9fed9e92113518fb9
Iain Hoolahan <iainhoolahan@outlook.com>
2023-07-13 11:49:09 +0100

    improved visibility of writing on home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,11 +3,11 @@ import { Link as ReachLink } from 'react-router-dom';
 import HomeBackground from '../Images/HomeBackground.jpg';
 
 const Home = () => {
-    const fontSizeA = useBreakpointValue({ base: '2xl', md: '5xl' });
-    const fontSizeB = useBreakpointValue({ base: '1xl', md: '2xl' });
+    const fontSizeA = useBreakpointValue({ base: '3xl', md: '5xl' });
+    const fontSizeB = useBreakpointValue({ base: '1xl', md: '3xl' });
 
     return (
-        <Box w="100%" h="100vh" position="relative" overflowX="visible">
+        <Box w="100%" h="100vh" position="relative" overflowX="visible" mt={{ base: '10px', md: '70px' }}>
             <Image src={HomeBackground} alt="Home Background" objectFit="cover" objectPosition="center" position="absolute" top={0} left={0} width="100%" height="100%" zIndex={-1} />
             <Box
                 position="absolute"
@@ -15,13 +15,24 @@ const Home = () => {
                 left="50%"
                 transform="translate(-50%, -50%)"
                 textAlign="center"
-                color="#184776"
             >
-                <Text fontSize={fontSizeA} fontWeight="bold" mb={4}>
+                <Text
+                    fontSize={fontSizeA}
+                    fontWeight="bold"
+                    mb={4}
+                    color="#184776"
+                    textShadow="2px 2px 4px rgba(0, 0, 0, 0.2)"
+                >
                     HARBOUR CONTRACTORS LTD
                 </Text>
                 <Box borderBottom="2px" borderColor="#184776" mx="auto" mb={4} width="100%" />
-                <Text fontSize={fontSizeB} fontWeight="bold" mb={6}>
+                <Text
+                    fontSize={fontSizeB}
+                    fontWeight="bold"
+                    mb={6}
+                    color="#184776"
+                    textShadow="2px 2px 4px rgba(0, 0, 0, 0.2)"
+                >
                     Offering design, build, and construction management services
                 </Text>
                 <RouterLink as={ReachLink} to="/contact" colorScheme="teal" _hover={{ textDecoration: 'none' }}>
@@ -35,3 +46,4 @@ const Home = () => {
 };
 
 export default Home;
+
