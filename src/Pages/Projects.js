a4973de3df2499b73481316be6a13b13d7a6abb4
Iain <iain.hoolahan@gmail.com>
2023-07-14 18:50:28 +0100

    adjust widths

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,19 +9,19 @@ const Projects = () => {
 
     return (
         <center>
-            <Box textAlign="center" p={4} mt="70px" width="100%">
+            <Box textAlign="center" p={4} mt="70px" width="90%">
                 <Heading as="h1" size="xl" my={8} color={textColor}>
                     Our Projects
                 </Heading>
 
-                <Box my={8} width="100%" maxWidth="100vw" overflowX="auto">
+                <Box my={8} width="100%" maxWidth="90vw" overflowX="auto">
                     <Text as="h2" size="md" color={textColor} pb="20px">
                         Trippets - Construction Phase - November 2020 - May 2022
                     </Text>
                     <Carousel>{renderCarouselItems('Trippets', 15)}</Carousel>
                 </Box>
 
-                <Box my={8} width="100%" maxWidth="100vw" overflowX="auto">
+                <Box my={8} width="100%" maxWidth="90vw" overflowX="auto">
                     <Text as="h2" size="md" color={textColor} pb="20px">
                         Java Sound - Construction Phase - September 2021 - December 2022
                     </Text>
@@ -31,7 +31,7 @@ const Projects = () => {
                 <style jsx>{`
                     .carousel-image {
                         max-height: 60vh;
-                        max-width: 100%;
+                        max-width: 95%;
                         height: auto;
                         object-fit: contain;
                     }
