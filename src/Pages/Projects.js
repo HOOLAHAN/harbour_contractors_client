504c2883cdb6f9bf5002fe2086fe299f6896791b
Iain <iain.hoolahan@gmail.com>
2023-07-14 18:41:53 +0100

    resolved heights for phone size

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,19 +9,19 @@ const Projects = () => {
 
     return (
         <center>
-            <Box textAlign="center" p={4} mt="70px" width="80%">
+            <Box textAlign="center" p={4} mt="70px" width="100%">
                 <Heading as="h1" size="xl" my={8} color={textColor}>
                     Our Projects
                 </Heading>
 
-                <Box my={8} width="100%">
+                <Box my={8} width="100%" maxWidth="100vw" overflowX="auto">
                     <Text as="h2" size="md" color={textColor} pb="20px">
                         Trippets - Construction Phase - November 2020 - May 2022
                     </Text>
                     <Carousel>{renderCarouselItems('Trippets', 15)}</Carousel>
                 </Box>
 
-                <Box my={8} width="100%">
+                <Box my={8} width="100%" maxWidth="100vw" overflowX="auto">
                     <Text as="h2" size="md" color={textColor} pb="20px">
                         Java Sound - Construction Phase - September 2021 - December 2022
                     </Text>
@@ -34,18 +34,6 @@ const Projects = () => {
                         max-width: 100%;
                         height: auto;
                         object-fit: contain;
-                    }
-
-                    @media screen and (min-width: 768px) {
-                        .carousel-image {
-                            max-width: 60%;
-                        }
-                    }
-
-                    @media screen and (max-width: 480px) {
-                        .carousel-image {
-                            max-width: 90vw;
-                        }
                     }
                 `}</style>
             </Box>
