18728d3dc9ae2fd02259ff70919af72e106054be
Iain <iain.hoolahan@gmail.com>
2023-07-19 11:48:15 +0100

    udpated ContactUs page to work with AWS Lambda endpoint

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -3,87 +3,76 @@ import { Box, Heading, Input, Textarea, Button, useColorMode } from '@chakra-ui/
 import axios from 'axios';
 
 const ContactUs = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: '',
-    });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    text_message: '',
+  });
 
-    const { colorMode } = useColorMode();
-    const isDarkMode = colorMode === 'dark';
-    const textColor = isDarkMode ? 'white' : '#184776';
+  const { colorMode } = useColorMode();
+  const isDarkMode = colorMode === 'dark';
+  const textColor = isDarkMode ? 'white' : '#184776';
 
-    const handleChange = (e) => {
-        setFormData((prevData) => ({
-            ...prevData,
-            [e.target.name]: e.target.value,
-        }));
-    };
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            await axios.post('/send-message', formData);
-            console.log('Message sent successfully');
-            // Add success message or redirect to a thank you page
-        } catch (error) {
-            console.log('Error sending message:', error);
-            // Handle error condition
-        }
-    };
+    try {
+      await axios.post('https://u1rgvdq1yi.execute-api.eu-west-2.amazonaws.com/sendMessage', {
+        name: formData.name,
+        email: formData.email,
+        text_message: formData.text_message,
+      });
 
-    const sendTwilioMessage = async () => {
-        try {
-            await axios.post('/send-twilio-message');
-            console.log('Twilio message sent successfully');
-            // Add success message or handle the response as needed
-        } catch (error) {
-            console.log('Error sending Twilio message:', error);
-            // Handle error condition
-        }
-    };
+      console.log('Message sent successfully');
+      // Show success message or redirect to a thank you page
+    } catch (error) {
+      console.log('Error sending message:', error);
+      // Handle error condition and display an error message to the user
+    }
+  };
 
-    return (
-        <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            height="100vh"
-        >
-        <Box maxWidth="500px" mx="auto" p={4} mt="70px">
-            <Heading as="h2" size="lg" mb={4} color={textColor}>
-                Contact Us
-            </Heading>
-            <form onSubmit={handleSubmit}>
-                <Input
-                    name="name"
-                    placeholder="Your Name"
-                    mb={4}
-                    value={formData.name}
-                    onChange={handleChange}
-                />
-                <Input
-                    name="email"
-                    placeholder="Your Email"
-                    mb={4}
-                    value={formData.email}
-                    onChange={handleChange}
-                />
-                <Textarea
-                    name="message"
-                    placeholder="Your Message"
-                    mb={4}
-                    value={formData.message}
-                    onChange={handleChange}
-                />
-                <Button mt={2} type="submit" colorScheme="teal" onClick={sendTwilioMessage}>
-                    Send
-                </Button>
-            </form>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box display="flex" alignItems="center" justifyContent="center" height="100vh">
+      <Box maxWidth="500px" mx="auto" p={4} mt="70px">
+        <Heading as="h2" size="lg" mb={4} color={textColor}>
+          Contact Us
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <Input
+            name="name"
+            placeholder="Your Name"
+            mb={4}
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <Input
+            name="email"
+            placeholder="Your Email"
+            mb={4}
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <Textarea
+            name="text_message"
+            placeholder="Your Message"
+            mb={4}
+            value={formData.text_message}
+            onChange={handleChange}
+          />
+          <Button mt={2} type="submit" colorScheme="teal">
+            Send
+          </Button>
+        </form>
+      </Box>
+    </Box>
+  );
 };
 
 export default ContactUs;
