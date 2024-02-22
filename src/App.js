1727e1d5aeed4826d52a6aa8ba2b01e0e0b2bd2e
Iain <iain.hoolahan@gmail.com>
2024-02-22 13:31:28 +0000

    added context wrapper to app

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import Services from './Pages/Services';
 import Projects from './Pages/Projects';
 
 import { Box, Grid } from '@chakra-ui/react';
+import { PreloadImageProvider } from './Context/PreloadImageContext';
 
 function App() {
     return (
-        <Box textAlign="center" fontSize="xl">
-            <Navbar />
-            <Grid minH="100vh">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/about" element={<AboutUs />} />
-                    <Route path="/services" element={<Services />} />
-                    <Route path="/projects" element={<Projects />} />
-                </Routes>
-            </Grid>
-            <Footer />
-        </Box>
+        <PreloadImageProvider>
+            <Box textAlign="center" fontSize="xl">
+                <Navbar />
+                <Grid minH="100vh">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<AboutUs />} />
+                        <Route path="/services" element={<Services />} />
+                        <Route path="/projects" element={<Projects />} />
+                    </Routes>
+                </Grid>
+                <Footer />
+            </Box>
+        </PreloadImageProvider>
     );
 }
 
