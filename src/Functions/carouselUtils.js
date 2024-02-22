51f923e755a6a05ddf0ff3c2c6b9ae88c57a3fa0
Iain <iain.hoolahan@gmail.com>
2024-02-22 11:44:38 +0000

    blur effect added to image when it loads

--- src/Functions/carouselUtils.js
+++ src/Functions/carouselUtils.js
@@ -1,4 +1,5 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export const renderCarouselItems = (prefix, count) => {
   const carouselItems = [];
