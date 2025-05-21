c729ab9bab6169bafc012b3ea97dc4cf8bc70274
Iain <iain.hoolahan@gmail.com>
2025-05-21 12:26:04 +0100

    preview images either side of carousel

--- src/Components/ProjectCarousel.js
+++ src/Components/ProjectCarousel.js
@@ -1,18 +1,15 @@
-// src/Components/ProjectCarousel.js
-
-import { Box, Center, IconButton, useToken, Image } from '@chakra-ui/react';
+import { Box, Center, IconButton, Image } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const ProjectCarousel = ({ images }) => {
-  const brandBg = useToken('colors', '#204775');
   const carouselRef = useRef(null);
 
   return (
     <Box my={6} maxW="90vw" mx="auto">
-      {/* Image Carousel */}
+      {/* Image Carousel with Previews */}
       <Carousel
         ref={carouselRef}
         showThumbs={false}
@@ -26,16 +23,21 @@ const ProjectCarousel = ({ images }) => {
         showArrows={false}
         renderArrowPrev={() => null}
         renderArrowNext={() => null}
+        centerMode
+        centerSlidePercentage={60} // Main image takes 60%, previews take 20% on either side
+        dynamicHeight={false}
       >
         {images.map((src, index) => (
-          <Box key={index}>
+          <Box key={index} display="flex" justifyContent="center">
             <Image
               src={src}
               alt={`Slide ${index + 1}`}
               objectFit="contain"
               maxH="60vh"
-              w="100%"
+              height="60vh"
+              width="auto"
               borderRadius="md"
+              mx="auto"
             />
           </Box>
         ))}
@@ -44,7 +46,7 @@ const ProjectCarousel = ({ images }) => {
       {/* Custom Arrow Controls Below */}
       {images.length > 1 && (
         <Center mt={4}>
-          <Box display="flex" gap={4}>
+          <Box display="flex" gap={8}>
             <IconButton
               icon={<ChevronLeftIcon boxSize={6} />}
               onClick={() =>
@@ -52,12 +54,12 @@ const ProjectCarousel = ({ images }) => {
                   (carouselRef.current.state.selectedItem - 1 + images.length) % images.length
                 )
               }
-              bg={brandBg}
+              bg="#204775"
               color="white"
               borderRadius="full"
               aria-label="Previous Slide"
-              _hover={{ bg: 'brand.200' }}
-              _active={{ bg: 'brand.100', transform: 'scale(0.95)' }}
+              _hover={{ bg: '#16395b' }}
+              _active={{ bg: '#122f4b', transform: 'scale(0.95)' }}
             />
             <IconButton
               icon={<ChevronRightIcon boxSize={6} />}
@@ -66,12 +68,12 @@ const ProjectCarousel = ({ images }) => {
                   (carouselRef.current.state.selectedItem + 1) % images.length
                 )
               }
-              bg={brandBg}
+              bg="#204775"
               color="white"
               borderRadius="full"
               aria-label="Next Slide"
-              _hover={{ bg: 'brand.200' }}
-              _active={{ bg: 'brand.100', transform: 'scale(0.95)' }}
+              _hover={{ bg: '#16395b' }}
+              _active={{ bg: '#122f4b', transform: 'scale(0.95)' }}
             />
           </Box>
         </Center>
